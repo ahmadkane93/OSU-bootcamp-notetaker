d77091c2480b48ca3acc51f4705c9349c1e61288
Ahmad Kane <82630118+ahmadkane93@users.noreply.github.com>
2021-07-22 02:23:37 -0400

    Update server(routing)

--- server.js
+++ server.js
@@ -3,15 +3,22 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express(); 
 
-require('./routes/apiroute');
-require('./routes/htmlroute');
-app.use(api);
-app.use(html);
+// Routing
+const apiRoute = require('./routes/apiroute');
+const htmlRoute = require('./routes/htmlroute');
+app.use(apiRoute);
+app.use(htmlRoute);
 
+
+// Parse Data
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
+
+
+// Activate server
 app.listen(PORT, function() {
     console.log(`Application's server is listening on PORT: ${PORT}`);
   });
+
