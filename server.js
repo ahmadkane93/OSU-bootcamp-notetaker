2b4cbc3c0bf3f5cca2b3153b262f8b7e9311fad4
Ahmad Kane <82630118+ahmadkane93@users.noreply.github.com>
2021-07-22 01:59:29 -0400

    update server

--- server.js
+++ server.js
@@ -3,5 +3,15 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express(); 
 
-require('')
-require('')
+require('./routes/apiroute');
+require('./routes/htmlroute');
+app.use(api);
+app.use(html);
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.listen(PORT, function() {
+    console.log(`Application's server is listening on PORT: ${PORT}`);
+  });
